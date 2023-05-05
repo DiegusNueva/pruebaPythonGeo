6b426ef90421cfab7d3530ae2c6444369106b9c1
Diego Alonso Molina <diegusnueva@gmail.com>
2023-05-05 13:37:12 +0200

    Añadimos la funcionalidad de calcular la distancia que existe entre dos ciudades

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 #render_template es el medoto que nos ayuda a llamar a los html's del proyecto
 from flask import Flask, render_template, request
 from geopy.geocoders import Nominatim
+import geopy.distance
 
 #inicializamos Flask (le decimos a Flask que este es el archivo principal. este archivo arranca la app)
 #esta variable se utiliza para crear las rutas del servidor, las URL's
@@ -21,10 +22,23 @@ def result():
 
     geo = Nominatim(user_agent="MyApp")
 
+    # Información relativa a los lugares
     nombreLugar = request.form["nombreLugar"]
+    nombreLugar2 = request.form["nombreLugar2"]
     loc = geo.geocode(nombreLugar)
+    loc2 = geo.geocode(nombreLugar2)
 
-    return render_template('result.html', address=loc.address, lat=loc.latitude, long=loc.longitude)
+    # Cálculo de la  distancia entre dos ciudades empleando su latitud y su longitud
+ 
+
+    ## Necesito sus coordenadas para calcular su distancia
+
+    coord_loc = (loc.latitude, loc.longitude)
+    coord_loc2 = (loc2.latitude, loc2.longitude)
+
+    distancia = geopy.distance.distance(coord_loc, coord_loc2)
+
+    return render_template('result.html', address=loc.address, lat=loc.latitude, long=loc.longitude, address2=loc2.address, lat2=loc2.latitude, long2=loc2.longitude, distancia=distancia)
 
 
 
